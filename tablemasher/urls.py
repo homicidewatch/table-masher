8d9e1944b51c72ad8f599c44a9389e3785945049
Chris Amico <eyeseast@gmail.com>
2011-01-10 18:48:48 -0500

    I hear static media is easier in 1.3

--- tablemasher/urls.py
+++ tablemasher/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import *
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -14,5 +15,11 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
     
-    url(r'^', include('tables.urls')),
+    url(r'^', include('tables.urls')),    
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        url(r'^static/(?P<path>.*)$', 'django.views.static.serve',
+            {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
+    )
